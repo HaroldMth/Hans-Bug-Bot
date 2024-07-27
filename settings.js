2cefd8df226917c811108bf748a035f5b65f40d1
HaroldMth <167818233+HaroldMth@users.noreply.github.com>
2024-07-27 03:51:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,13 +9,13 @@ const chalk = require('chalk')
 
 //contact details
 global.ownernomer = "237696900612"
-global.ownername = "*Hans+Tech"
+global.ownername = "*Hans Tech"
 global.ytname = "YT: HansTech0"
 global.socialm = ""
 global.location = "EARTH,AFRICA"
 
 global.ownernumber = '237696900612'  //creator number
-global.ownername = 'Harold+MTH' //owner name
+global.ownername = 'Harold MTH' //owner name
 global.botname = 'Hans-Bug-bot' //name of the bot
 
 //sticker details
