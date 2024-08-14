d4bec42252962f323f7d55aac8546eccdc74a059
Khalidalhaker <113259512+khalid-Hake@users.noreply.github.com>
2024-08-14 10:47:31 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,13 +8,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "237696900612"
+global.ownernomer = "50941654720"
 global.ownername = "*Hans Tech"
 global.ytname = "YT: HansTech0"
 global.socialm = ""
 global.location = "EARTH,AFRICA"
 
-global.ownernumber = '237696900612'  //creator number
+global.ownernumber = '50941654720'  //creator number
 global.ownername = 'Harold MTH' //owner name
 global.botname = 'Hans-Bug-bot' //name of the bot
 
