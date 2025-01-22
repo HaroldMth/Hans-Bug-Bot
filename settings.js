7fb4089c449c0158d8714470a583d171d9062bbe
Frozy <146687482+Soldierboy2023@users.noreply.github.com>
2025-01-22 01:55:16 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,19 +8,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "237696900612"
+global.ownernomer = "4915172861284"
 global.ownername = "*Hans Tech"
 global.ytname = "YT: HansTech0"
 global.socialm = ""
 global.location = "EARTH,AFRICA"
 
-global.ownernumber = '237696900612'  //creator number
+global.ownernumber = '4915172861284'  //creator number
 global.ownername = 'Harold MTH' //owner name
 global.botname = 'Hans-Bug-bot' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By Hans Tech'
-global.author = 'Hallo\n\nContact: 237696900612'
+global.author = 'Hallo\n\nContact: 4915172861284'
 
 //console view/theme
 global.themeemoji = '😎'
